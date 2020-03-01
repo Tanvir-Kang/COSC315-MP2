0407ea9e1c84ce29481598a5841bb18271acc988
tanvirkang <tanvirkang@localhost>
2020-02-29 16:18:20 -0800

    Added getInput() function to main

--- COSC315-MP2/src/pkg/Main.java
+++ COSC315-MP2/src/pkg/Main.java
@@ -1,13 +1,28 @@
 package pkg;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Request r1 = new Request(1,5);
 		r1.info();
-		System.out.println(r1.getId());
-		System.out.println(r1.getLengthReq());
+		getInput();
+	
+	}
+	
+	public static void getInput() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Info: \nN = The number of slave threads and size of input buffer (INT). M = Max seconds of any one request can take(N)");
+		
+		while(true) {
+			
+		}
+		System.out.println("Please enter N: followed by enter");
+		int N = in.nextInt();
 
+		System.out.println("Please enter M: followed by enter");
+		int M = in.nextInt();
 	}
 
 }
